e59be1273a7d116167c05c2461942a0f055dc230
Keyuri Thakkar <-GitHub-email>
2026-09-15 16:14:21 +0530

    Update GlobeTrotter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate
-} from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import LoginScreen from "./components/LoginScreen";
 import Dashboard from "./components/Dashboard";
